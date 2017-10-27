45b094379d8d901a708d742ec2da4821e4e4d153
ctinray <b03902072@ntu.edu.tw>
2017-10-27 10:36:56 +0800

    fix rnn model path

--- hw1/run_rnn.sh
+++ hw1/run_rnn.sh
@@ -1,1 +1,1 @@
-python3.6 src/predict_test.py "${1}" model/mean-std-mfcc-new.pickle model/rnn-10.63841/ "${2}" --model rnn --gpu_mem 1
+python3.6 src/predict_test.py "${1}" model/mean-std-mfcc-new.pickle model/rnn-mfcc-10.63841/ "${2}" --model rnn --gpu_mem 1
