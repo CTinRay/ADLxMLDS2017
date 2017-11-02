fix loss average

hw2/src/s2vd.py
@@ -5,7 +5,7 @@ import pdb
 
 use_cuda = torch.cuda.is_available()
 class S2VD(torch.nn.Module):
-    def __init__(self, frame_dim, word_dim, hidden_size=256):
+    def __init__(self, frame_dim, word_dim, hidden_size=768):
         super(S2VD, self).__init__()
 
         self.gru_video = torch.nn.GRU(frame_dim,
@@ -55,7 +55,7 @@ class S2VD(torch.nn.Module):
         # init prev_pred with <sos>
         predicts = torch.zeros(batch_size,
                                max(batch['lengths']))
-        predicts[:, 0] = 1
+        predicts[:, 0] = 2
         predicts.cuda()
 
         loss = Variable(torch.zeros(1).cuda())

hw2/src/train.py
@@ -44,7 +44,8 @@ def main():
     arch = archs[args.arch](frame_dim, word_dim)
     arch.cuda()
     clf = TorchWrapper(arch,
-                       torch.nn.CrossEntropyLoss(class_weights.cuda()),
+                       torch.nn.CrossEntropyLoss(class_weights.cuda(),
+                                                 size_average=False),
                        batch_size=args.batch_size,
                        valid=test,
                        n_epochs=args.n_epochs,
@@ -52,8 +53,11 @@ def main():
 
     model_checkpoint = ModelCheckpoint(args.model_path,
                                        'loss', 1, 'min')
-    print_predict = PrintPredict(train, data_processor)
-    clf.fit_dataset(train, [model_checkpoint, print_predict])
+    print_predict_train = PrintPredict(train, data_processor)
+    print_predict_test = PrintPredict(test, data_processor)
+    clf.fit_dataset(train, [model_checkpoint,
+                            print_predict_train,
+                            print_predict_test])
 
 
 if __name__ == '__main__':