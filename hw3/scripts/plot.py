c3f2f3d6aacc28a49cd461886877a4baea341cd9
ctinray <b03902072@ntu.edu.tw>
2017-12-10 21:40:52 +0800

    plot many lines on same figure

--- hw3/scripts/plot.py
+++ hw3/scripts/plot.py
@@ -7,7 +7,7 @@ import numpy as np
 def read_file(filename):
     timesteps = []
     rewards = []
-    with open(args.log_file) as f:
+    with open(filename) as f:
         for l in f:
             cols = l.split(',')
             timesteps.append(float(cols[0]))
@@ -17,31 +17,37 @@ def read_file(filename):
 
 
 def main(args):
-    timesteps, rewards = read_file(args.log_file)
-    timesteps = np.array(timesteps)
-    rewards = np.array(rewards)
-    rewards = np.convolve(rewards,
-                          np.ones((args.window_size,)) / args.window_size,
-                          mode='valid')
     fig = plt.figure(dpi=450)
     ax = fig.add_subplot(1, 1, 1)
     ax.set_title('Reward over time')
     ax.set_xlabel('Time Steps')
     ax.set_ylabel('Avg. Reward')
 
-    index_start = args.window_size // 2
-    index_end = len(timesteps) - index_start + 1
-    ax.plot(timesteps[index_start:index_end], rewards)
+    for i in range(len(args.log_file)):
+        timesteps, rewards = read_file(args.log_file[i])
+        timesteps = np.array(timesteps)
+        rewards = np.array(rewards)
+        rewards = np.convolve(rewards,
+                              np.ones((args.window_size,)) / args.window_size,
+                              mode='valid')
+        index_start = args.window_size // 2
+        index_end = len(timesteps) - index_start + 1
+
+        label = args.labels[i] if args.labels is not None else None
+        ax.plot(timesteps[index_start:index_end], rewards, label=label)
+
+    ax.legend()
     fig.savefig(args.output)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Plot learning curve.')
-    parser.add_argument('log_file', type=str,
+    parser.add_argument('log_file', type=str, nargs='+',
                         help='Path to the log file.')
     parser.add_argument('output', type=str,
                         help='Directory of the data.')
     parser.add_argument('--window_size', type=int, default=100)
+    parser.add_argument('--labels', type=str, nargs='+', default=None)
     args = parser.parse_args()
     return args
 
