923d8dee6ef643c9450c1a7dcd98c4d0fd3a5e40
ctinray <b03902072@ntu.edu.tw>
2018-01-06 16:40:10 +0800

    fix run.sh script

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,1 +1,1 @@
-python src/generate.py models/iwgan-12-28-15/model-epoch-435 "${1}" samples --random_seed 1
+python src/generate.py models/iwgan-12-28-15/model-epoch-435 "${1}" samples --rand_seed 1
